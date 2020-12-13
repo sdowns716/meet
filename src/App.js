661322370adb43c167b53e88e7e6b69e915e551f
sdowns716 <66967392+sdowns716@users.noreply.github.com>
2020-12-13 15:45:44 -0500

    pie chart

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { extractLocations, getEvents } from './api';
 import "./nprogress.css";
+import EventGenre from './EventGenre';
 import { OfflineAlert } from "./Alert";
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+} from 'recharts';
+
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
     NumberOfEvents: 32, 
-    currentLocation: "all"
+    currentLocation: "all",
+    alertText: "",
   };
 
   
@@ -74,17 +80,49 @@ class App extends Component {
     this.mounted = false;
   }
 
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(' ').shift()
+      return {city, number};
+    })
+    return data;
+  };
+
   render() {
+    const { locations, numberOfEvents, events } = this.state;
     return (
       <div className="App">
-         <OfflineAlert text={this.state.alertText} />
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <EventList events={this.state.events} />
+        <h1>Meet App</h1>
+        <OfflineAlert text={this.state.alertText} />
+        <h4>Choose your nearest city</h4>
+        <CitySearch updateEvents={this.updateEvents} locations={locations} />
         <NumberOfEvents
-            updateEvents={this.updateEvents}
-            numberOfEvents={this.state.numberOfEvents} />
-      </div>
-    );
-  }
+          updateEvents={this.updateEvents}
+          numberOfEvents={numberOfEvents}
+        />       
+        <div className="data-vis-wrapper">
+          <EventGenre 
+            events={this.state.events} />
+          <ResponsiveContainer height={400} >
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis
+              allowDecimals={false}
+              type="number"
+              dataKey="number"
+              name="number of events"
+            />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+      </div>  
+    <EventList events={events} />
+  </div>
+);
 }
+} 
 export default App;
